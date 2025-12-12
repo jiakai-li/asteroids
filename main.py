4e2def1540eede84c69f8798d02a42211c01850d
Jiakai Li <li.jiakai@hotmail.com>
2025-12-12 21:56:08 +0000

    asteroids wip

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -9,6 +10,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -18,7 +25,14 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+
+        since_last = clock.tick(60)
+        dt = since_last / 1000
 
 
 if __name__ == "__main__":
