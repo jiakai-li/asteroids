7b4d8cc4eb3279ed21b67cd9b6b70e536eca6eb5
Jiakai Li <li.jiakai@hotmail.com>
2025-12-13 17:34:07 +0000

    asteroids wip with split

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
+import random
 import pygame
 
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -13,3 +15,20 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        first_velocity = self.velocity.rotate(random_angle)
+        second_velocity = self.velocity.rotate(-1 * random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        child_one = Asteroid(self.position[0], self.position[1], new_radius)
+        child_one.velocity = first_velocity * 1.2
+
+        child_two = Asteroid(self.position[0], self.position[1], new_radius)
+        child_two.velocity = second_velocity * 1.2
